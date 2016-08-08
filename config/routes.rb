4dd53c6fb586406a65354c44e3d091523d87a7ea
Mat <geilermathieu@gmail.com>
2016-08-08 12:46:31 +0200

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'ask', to: 'pages#ask'
+
+  get 'answer/:query', to: 'pages#answer'
+
+  get 'answer', to: 'pages#answer'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
